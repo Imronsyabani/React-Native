d9b10e611a7552d5ee440dd5915d561a262b709e
imronsyabani <imoehsawa67@gmail.com>
2024-11-17 13:01:38 +0700

    update fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import "../global.css";
 const RootLayout = () => {
     return (
-        <SafeAreaView style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1, width: "auto"}}>
             <Stack screenOptions={{headerShown: false}}>
                 <Stack.Screen name="index"/>
             </Stack>
--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,9 @@
-import { View, Text, FlatList, Image } from "react-native";
-
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 const dataMovie = [{
     id: "1234",
     title: "Avenger End Game",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit . Totam suscipit tenetur mollitia alias dignissimos dolorem expedita pariatur  dicta reiciendis excepturi dolor voluptates enim voluptas ullam accusantium numquam fuga, molestias eligendi natus. Eos temporibus fuga nostrum magnam a quo alias tenetur? Tempore, laudantium nam dolores et repudiandae ex in tenetur quibusdam.",
     rating: 8.9
 }]
 
@@ -18,19 +19,35 @@ const Header = () => {
     )
 }
 
-const ListMove = () => {
+const ListMovie = ({item}) => {
     return (
-        <View className="bg-base-1">
-            <View></View>
+        <View className="bg-white justify-start flex-row mt-6 p-1">
+            <View>
+                <Image 
+                source={{uri: "https://m.media-amazon.com/images/M/MV5BNzUyM2YyY2MtNzNlMS00MWU5LTgxNjAtNzZlNmI2NjU2NDZlXkEyXkFqcGc@._V1_SX300.jpg"}}
+                className="w-40 h-52 resize-y"
+                />
+            </View>
+            <View className="flex-col justify-start items-start">
+                <View>
+                    <Text>{item.title}</Text>
+                </View>
+                <View>
+                    <Text 
+                    adjustsFontSizeToFit={true}
+                    numberOfLines={50}
+                    accessible={true}>{item.description}</Text>
+                </View>
+            </View>
         </View>
     )
 }
-
 const Index = () => {
     return (
         <FlatList
         data={dataMovie}
-
+        className="bg-base-1"
+        renderItem={({item}) => <ListMovie item={item}/>}
         keyExtractor={move => move.id}
         ListHeaderComponent={<Header />}
         />
